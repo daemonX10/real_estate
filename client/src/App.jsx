8c54b7b29b9d272ffdb48edd4cffa41176aad3d6
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-28 18:17:20 +0530

    layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { Suspense } from "react";
 import "./App.css"
+import Website from "./pages/Website"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 
 function App() {
     return(
-      <Website/>
+      <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+
+      <Routes>
+
+        <Route element={<Layout/>}>
+            <Route path="/" element={<Website/>}/>
+        </Route>
+        
+      </Routes>
+      </Suspense>
+      </BrowserRouter>
     );
 }
 
