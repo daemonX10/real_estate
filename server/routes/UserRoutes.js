2e77fc6a970278412903c0ad92e9f73c11dfb3fd
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-28 16:52:03 +0530

    user model

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { createUser } from '../controllers/userController.js';
+import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFav, toFav } from '../controllers/userController.js';
 
 router.post("/register", createUser);
+router.post("/bookvisit/:id", bookVisit )
+router.post("/allBookings", getAllBookings)
+router.post('/removeBooking/:id',cancelBooking);
+router.post('/toFav/:rid',toFav);
+router.post('/allFav',getAllFav);
 
 
 export {router as userRoutes}
